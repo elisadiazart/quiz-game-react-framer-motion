eb9fc7373304e6bef82b7de81c4e4e5ab1224174
Elisa <elisadiazartcontact@gmail.com>
2023-05-04 16:23:20 +0200

    04.05.23

--- src/components/questions/Questions.jsx
+++ src/components/questions/Questions.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import {  useState } from 'react';
 import { QUESTIONS } from '../../constants/questions';
 import Question from '../question/Question';
 import { v4 } from 'uuid';
@@ -7,11 +7,12 @@ import { StyledQuestions, StyledMain } from './styles';
 const Questions = () => {
 	const [currentQuestion, setCurrentQuestion] = useState(0);
 
+
 	return (
 		<StyledMain
 			initial={{ opacity: 0, scale: 0 }}
 			animate={{ opacity: 1, scale: 1 }}
-			transition={{ duration: 1.5 }}
+			transition={{ duration: 1 }}
 		>
 			<h2>{QUESTIONS[currentQuestion].question}</h2>
 			<StyledQuestions>
@@ -19,7 +20,7 @@ const Questions = () => {
 					<Question
 						key={v4()}
 						text={answer}
-						handleClick={() => setQuestion(currentQuestion, setCurrentQuestion)}
+						handleClick={() => setCurrentQuestion(currentQuestion + 1)}
 					/>
 				))}
 			</StyledQuestions>
@@ -27,9 +28,9 @@ const Questions = () => {
 	);
 };
 
-const setQuestion = (currentQuestion, setCurrentQuestion) => {
-	setCurrentQuestion(currentQuestion + 1);
-	console.log(currentQuestion);
-};
+// const setQuestion = (currentQuestion, setCurrentQuestion) => {
+// 	setCurrentQuestion(currentQuestion + 1);
+// 	console.log(currentQuestion);
+// };
 
 export default Questions;
