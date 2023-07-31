62c70903ecb3bc2fb2c66c3fc0fbe70d74174daa
Elisa <elisadiazartcontact@gmail.com>
2023-07-31 18:21:06 +0200

    31.07.23 (Con delay)

--- src/components/question/Question.jsx
+++ src/components/question/Question.jsx
@@ -2,7 +2,11 @@ import { StyledButton, StyledText } from './styles';
 
 const Question = ({ text, handleClick, index }) => {
 	return (
-		<StyledButton onClick={handleClick} index={index}>
+		<StyledButton onClick={handleClick} index={index}
+			initial={{ opacity: 0, scale: 0 }}
+			animate={{ opacity: 1, scale: 1 }}
+			transition={{ duration: 1, delay: index - 0.2 }}
+		>
 			<StyledText>{text}</StyledText>
 		</StyledButton>
 	);
--- src/components/questions/Questions.jsx
+++ src/components/questions/Questions.jsx
@@ -5,22 +5,21 @@ import { v4 } from 'uuid';
 import { StyledQuestions, StyledMain } from './styles';
 
 const Questions = () => {
-	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [currentQuestion] = useState(0);
 
 
 	return (
-		<StyledMain
+		<StyledMain>
+			<h2>{QUESTIONS[currentQuestion].question}</h2>
+			<StyledQuestions 
 			initial={{ opacity: 0, scale: 0 }}
 			animate={{ opacity: 1, scale: 1 }}
-			transition={{ duration: 1 }}
-		>
-			<h2>{QUESTIONS[currentQuestion].question}</h2>
-			<StyledQuestions>
-				{QUESTIONS[currentQuestion].options.map(answer => (
+			transition={{ duration: 1 }}>
+				{QUESTIONS[currentQuestion].options.map((answer, index) => (
 					<Question
 						key={v4()}
 						text={answer}
-						handleClick={() => setCurrentQuestion(currentQuestion + 1)}
+						index={index}
 					/>
 				))}
 			</StyledQuestions>
@@ -28,9 +27,6 @@ const Questions = () => {
 	);
 };
 
-// const setQuestion = (currentQuestion, setCurrentQuestion) => {
-// 	setCurrentQuestion(currentQuestion + 1);
-// 	console.log(currentQuestion);
-// };
+
 
 export default Questions;
